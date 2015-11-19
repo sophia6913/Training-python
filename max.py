76f7b916a2b770d7c39eaccd0329285869bc26f0
Sophia Hsieh <sophiahsieh@kkbox.com>
2015-11-19 14:28:55 +0800

    Modified max.py

--- max.py
+++ max.py
@@ -1,16 +1,24 @@
 import sys
 count = 3
-with open(sys.argv[1]) as f:
-	b = [0, 0, 0]
+b = [0] * count	
+
+def mysort(mylist):
+	print "mylist:", mylist
+	length = len(mylist)-1
+	while length > 0:
+		for i in range(length):
+			if mylist[i] > mylist[i+1]:
+				mylist[i], mylist[i+1] = mylist[i+1], mylist[i]		
+		length -= 1
+	return mylist
+
+
+with open('input.txt') as f:
 	a = [int(line) for line in f]
 
-flag = 0
-while not flag:
-	flag = 1
-	for i in range(0, len(a)-1):
-		if a[i] > a[i+1]:
-			flag = 0
-			a[i], a[i+1] = a[i+1], a[i]
-
-print a[::-1][:count]
+for i in a:
+		if i > b[0]:
+			b[0] = i
+			b = mysort(b)
+print b[::-1]
 	
