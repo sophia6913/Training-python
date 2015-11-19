a7b7251a8985ac62261e7acc3f31e979dbcfb29f
Sophia Hsieh <sophiahsieh@kkbox.com>
2015-11-19 15:00:52 +0800

    Modified the sort part of max.py

--- max.py
+++ max.py
@@ -1,7 +1,7 @@
 import sys
 count = 3
 b = [0] * count	
-
+'''
 def mysort(mylist):
 	print "mylist:", mylist
 	length = len(mylist)-1
@@ -11,14 +11,21 @@ def mysort(mylist):
 				mylist[i], mylist[i+1] = mylist[i+1], mylist[i]		
 		length -= 1
 	return mylist
-
+'''
+def mysort(mylist):
+	length = len(mylist)-1
+	for i in range(length):
+    		for j in range(length - i):
+        		if mylist[j] > mylist[j+1]:
+   	         		mylist[j], mylist[j+1] = mylist[j+1], mylist[j]
+	return mylist
 
 with open('input.txt') as f:
 	a = [int(line) for line in f]
 
 for i in a:
-		if i > b[0]:
-			b[0] = i
-			b = mysort(b)
+	if i > b[0]:
+		b[0] = i
+		b = mysort(b)
 print b[::-1]
 	
